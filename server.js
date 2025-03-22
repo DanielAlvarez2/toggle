49a73e94ef0c3c47efcd9270c97bb374bbf514ed
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-03-22 01:12:57 -0400

    create contact form

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
-
+const nodemailer = require('nodemailer')
 (async ()=>{
     try{
         await mongoose.connect(process.env.MONGODB_URI)
@@ -16,5 +16,16 @@ app.use(express.static('public'))
 app.use(express.json())
 
 app.get('/', async(req,res)=>res.render('index.ejs'))
+app.get('/contact', async(req,res)=>res.render('contact.ejs'))
+app.post('/form2email', async(req,res)=>{
+    try{
+        const info = `
+        <h1>Message from ToggleSoftware.com</h1>
+        
+        `
+    }catch(err){
+        console.log(err)
+    }
+})
 
 app.listen(process.env.PORT, ()=> console.log(`Server Running on Port: ${process.env.PORT}`))
