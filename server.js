f6bb4becabd73ec208e03a2c5167af044997a54c
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-03-25 01:55:55 -0400

    change const to let for Set

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({extended:true}))
 
 
 
-const uniqueIPs = new Set
+let uniqueIPs = new Set
 let IPlist = []
 app.use(async (req,res,next)=>{
     console.log('current IP: ' + requestIP.getClientIp(req))
